79950b8faeab53d61706765e671eef2a75526b3c
sallebruno <9392@holbertonstudents.com>
2025-01-02 15:05:28 +0000

    update files

---
+++ jbk_shell.c
@@ -0,0 +1,54 @@
+#include "shell.h"
+
+/**
+ * read_input - Reads a line of input from the user
+ *
+ * Return: Pointer to the input string
+ */
+char *read_input(void)
+{
+    char *input = NULL;
+    size_t len = 0;
+
+    if (getline(&input, &len, stdin) == -1) // Read input
+    {
+        return NULL; // Return NULL on EOF or error
+    }
+
+    return input; // Return the input string
+}
+
+/**
+ * execute_command - Executes a command
+ * @input: Command input
+ */
+void execute_command(char *input)
+{
+	pid_t pid;
+	int status;
+	char *command = strtok(input, "\n");
+	if (command == NULL || strlen(command) == 0)
+	{
+		return;
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return;
+	}
+
+	if (pid == 0)
+	{
+		if (execve(command, NULL, NULL) == -1)
+	{
+		fprintf(stderr, "./jbk_shell: No such file or directory\n");
+		exit(EXIT_FAILURE);
+	}
+	}
+	else
+	{
+		wait(&status);
+	}
+}
