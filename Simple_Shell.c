c11de06b98e5512b6685a3816b9bf0fa0767c43e
Jonathan Pérez <135724908+Jonatha32@users.noreply.github.com>
2025-01-06 16:53:37 -0300

    Update Simple_Shell.c

--- Simple_Shell.c
+++ Simple_Shell.c
@@ -14,21 +14,20 @@
 
 char *find_comm(char *command, char *path)
 {
-	char *copia_path = strdup(path); /* duplicamos PATH */
-	char *dirpath = strtok(copia_path, ":"); /* Separamos en directorios*/
-	char full_path[1024]; /* Donde almacenaremos la ruta del comando */
-	char *resultado = NULL; /* Resultado que pasaremos a execute_command */
+	char *copia_path = strdup(path);
+	char *dirpath = strtok(copia_path, ":");
+	char full_path[1024];
+	char *resultado = NULL;
 
 	while (dirpath != NULL)
 	{
 	snprintf(full_path, sizeof(full_path), "%s/%s", dirpath, command);
-			/* creamos la ruta con el formato "dirpath/command" */
-		if (access(full_path, X_OK) == 0) /* Si existe y es ejecutable */
+		if (access(full_path, X_OK) == 0)
 		{
-		resultado = strdup(full_path); /* duplicamos y almacenamos el resultado */
+		resultado = strdup(full_path);
 		break;
 		}
-		dirpath = strtok(NULL, ":"); /* pasamos al siguiente token */
+		dirpath = strtok(NULL, ":");
 	}
 	free(copia_path);
 	return (resultado);
@@ -42,37 +41,35 @@ char *find_comm(char *command, char *path)
 
 void exe_com(char *line, char *path)
 {
-	char *argumentos[100], *token; /*variables para los argumentos que se pasan*/
-	char *full_path; /*Ruta Completa */
+	char *argumentos[100], *token; 
+	char *full_path;
 	int i = 0;
 
 	token = strtok(line, " ");
-	/*Dividimos la linea mediante " " para tratar cada palabra individualmente */
 	while (token != NULL && i < 99)
 	{
-	argumentos[i++] = token; /*Los tokens se almacenan en la variable argumentos*/
-	token = strtok(NULL, " "); /* Pasa al siguiente token */
+	argumentos[i++] = token; 
+	token = strtok(NULL, " ");
 	}
-	argumentos[i] = NULL; /*Al final se agrega el NULL para terminar el string*/
+	argumentos[i] = NULL; 
 	full_path = find_comm(argumentos[0], path);
-	/*Se llama a la funcion find_command y se almacena el resultado en full_path*/
-	if (full_path == NULL) /* se trata el caso de que full_path sea NULL */
+	if (full_path == NULL)
 	{
 		printf("%s: Command not found\n", argumentos[0]);
 		return;
 	}
-	pid_t pid = fork(); /* Creamos un proceso */
-			if (pid == 0) /* Proceso hijo que ejecute el comando */
+	pid_t pid = fork();
+			if (pid == 0)
 			{
 				execve(full_path, argumentos, environ);
 				perror("Execution Failed");
 				exit(EXIT_FAILURE);
 			}
-			else if (pid > 0) /* Proceso Padre, que espera al hijo */
+			else if (pid > 0)
 			{
 				wait(NULL);
 			}
-			else /* Error al crear el proceso */
+			else
 			{
 				perror("Fork Failed");
 			}
@@ -91,26 +88,25 @@ int main(int argc, char *argv[])
 	size_t len = 0;
 	char *line = NULL, *path = getenv("PATH");
 
-	if (path == NULL) /* Si PATH es NULL */
-	{
+	if (path == NULL) 
 		perror("Failed to get PATH");
 		return (1);
 	}
 
 	while (1)
 	{
-		printf("Simple_Shell$ "); /*Se muestra el prompt */
-		bytes_read = getline(&line, &len, stdin); /* Usamos getline */
-		if (bytes_read == -1) /* Caso Error getline */
+		printf("Simple_Shell$ ");
+		bytes_read = getline(&line, &len, stdin);
+		if (bytes_read == -1)
 		{
 			perror("Read Line Error");
 			break;
 		}
 
-		line[strcspn(line, "\n")] = '\0'; /* Eliminamos el posible salto de linea */
-		if (strcmp(line, "exit") == 0) /* Por si escribe "Exit" */
+		line[strcspn(line, "\n")] = '\0';
+		if (strcmp(line, "exit") == 0)
 			break;
-		exe_com(line, path); /* Llamamos a exe_comm */
+		exe_com(line, path);
 	}
 	free(line);
 	return (0);
